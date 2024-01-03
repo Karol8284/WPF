5d596c3bd720c156b93fdb5bdd8ce7f6edb89b80
Karol8284 <93425066+Karol8284@users.noreply.github.com>
2024-01-03 12:07:13 +0100

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using System.Text;
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            //sConsole.WriteLine("HELLO world");
+        }
+        
+    }
+}
